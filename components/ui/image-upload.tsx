ef2d558574cec140e9972efaa19d539a6efff0f4
Ilham Fikri <ilhamfikri@Ilhams-MacBook-Air.local>
2024-07-26 08:27:08 +0700

    cms online shop

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -55,7 +55,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="gtprd9am">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="qcxfesxr">
         {({ open }) => {
           const onClick = () => {
             open();
